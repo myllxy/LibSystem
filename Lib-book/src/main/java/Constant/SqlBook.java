23c5b21efd738ac8a4d02bbba0950ed3632512d0
tom <tom@qq.com>
2019-09-09 15:28:03 +0800

    修改常量

--- Lib-book/src/main/java/Constant/SqlBook.java
+++ Lib-book/src/main/java/Constant/SqlBook.java
@@ -10,9 +10,9 @@ import Book.BookJTable;
 public enum SqlBook implements Val {
     SELECT("select number,stackroom from book where bookname=?;"),
     UPDATE("update student set Totallend = Totallend + 1,Notreturned = Notreturned + 1 where StudentId=?;"),
-    INSERT_BOOK("insert into book values(?,'','');"),
+    INSERT_BOOK_1("insert into book values(?,'','');"),
     SELECT_BOOK("select * from book;"),
-    DELECT_BOOK("delete from book where number=?"),
+    DELECT_BOOK_1("delete from book where number=?"),
     UPDATE_BOOK_BOOKNAME("update book set bookname=? where bookname=? and number =?;"),
     UPDATE_BOOK_STACKROOM("update book set stackroom=? where stackroom=? and number =?;")
     ;
